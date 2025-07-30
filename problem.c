61ea1df61d499e246008642df3229950959cb6e1
Kazuya Nomura <knomura@c4r3s9.42tokyo.jp>
2025-07-30 17:01:55 +0900

    256 B問題

--- problem.c
+++ problem.c
@@ -1,18 +1,61 @@
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int n;
+// 	scanf("%d", &n);
+
+// 	int a[n + 1];
+// 	for (int i = 0; i < n; i++)
+// 		scanf("%d", &a[i]);
+
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		for (int j = 0; j < i; j++)
+// 		{
+// 			a[j] += a[i];
+// 		}
+// 	}
+// 	int count = 0;
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		if (a[i] >= 4)
+// 			count++;
+// 	}
+// 	printf("%d", count);
+// 	return (0);
+// }
+
+
 #include <stdio.h>
 
-int main()
-{
-	int l, r;
-	scanf("%d %d", &l, &r);
+int main(void) {
+  int N;
+  scanf("%d", &N);
 
-	if (l && r || !l && !r)
-	{
-		printf("Invalid");
-		return (0);
-	}
-	if (l)
-		printf("Yes");
-	if (r)
-		printf("No");
+  int a[N];
+  for (int i = 0; i < N; i++)
+    scanf("%d", &a[i]);
+
+  int count = 0;
+  int sum_of = 0;
+
+  int temp = N;
+  N = N - 1;
+
+  sum_of += a[N--];
+  while (sum_of < 4 && N >= 0)
+  {
+	count++;
+    sum_of += a[N];
+	N--;
+  }
+  if (N < 0)
+  {
+	printf("0");
 	return (0);
+  }
+
+  printf("%d", temp - count);
+  return (0);
 }
